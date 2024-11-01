8d5fdce839c58906fb907109efaf5f7a65df1bb1
Anyelo Guerrero <angelo.guerrero90@gmail.com>
2024-11-01 16:42:52 -0500

    se actualiza el consumo de la api

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 
-const link = "https://jsonplaceholder.typicode.com/users"
+// const link = "https://jsonplaceholder.typicode.com/users"
 
-const api = async () => {
-    const respuesta = await fetch(link)
-    const data = await respuesta.json()
-    console.log(respuesta)
-    console.log(data)
-}
+// const api = async () => {
+//     const respuesta = await fetch(link)
+//     const data = await respuesta.json()
+//     console.log(respuesta)
+//     console.log(data)
+// }
 
-api()
+// api()
+
+
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+.then((respuesta) => respuesta.json())
+.then((json) => console.log(json))
+
