8d9f3254090e2a5b463e138919ec96a6409b433d
Anyelo Guerrero <angelo.guerrero90@gmail.com>
2024-11-02 09:56:39 -0500

    se agrega iteracion de array en script.js

--- script.js
+++ script.js
@@ -11,15 +11,17 @@
 // api()
 
 
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-.then((respuesta) => respuesta.json())
-.then((json) => console.log(json))
+// fetch('https://jsonplaceholder.typicode.com/posts/1')
+// .then((respuesta) => respuesta.json())
+// .then((json) => console.log(json))
 
 
-const animals = ["cat","dog","cow","pig"]
+const animals = ["Cat","Dog","Cow","Pig"]
 
-function arr(){
-    for(i=0; i<arr.length ;i++){
-     
+function arr(arr1){
+    for(i=0; i<arr1.length ;i++){
+     console.log(arr1[i])
     }
 }
+
+arr(animals)
